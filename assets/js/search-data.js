7b1f9f60503a3ade0c12d758fb4b471ad529dd7e
MukundhMurthy <mukundh.murthy@icloud.com>
2025-08-15 18:10:21 +0000

    Deploying to gh-pages from @ MukundhMurthy/mukundhmurthy.github.io@3c5b1c1a54a295f39facc05109f2b82af95c7a64 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,40 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+        },{id: "nav-travel",
+          title: "travel",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
+            window.location.href = "/travel/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
@@ -71,6 +43,13 @@ ninja.data = [{
               section: "Dropdown",
               handler: () => {
                 window.location.href = "/blog/";
+              },
+            },{id: "dropdown-travel",
+              title: "travel",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/travel/";
               },
             },{id: "post-a-post-with-plotly-js",
         
